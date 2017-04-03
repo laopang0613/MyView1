21d2d29e1ef3cc0f8aeaad711a1fc3a746da7925
laopang0613 <327654545@qq.com>
2017-04-03 13:41:07 +0800

    从本地导入书籍后返回主界面，异步读取数据库，

--- app/src/main/java/com/example/pangyinglei/myview1/MainActivity.java
+++ app/src/main/java/com/example/pangyinglei/myview1/MainActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +25,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -78,7 +82,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         //安卓6.0以前的版本不需要检查权限。
         if(currSDKVersion < 23){
             createAppFold();
-            startReadDBTask();
+            //startReadDBTask();
+            Message msg = handler.obtainMessage();
+            handler.sendMessage(msg);
         }
         else {
             int permissionCheck1 = ContextCompat.checkSelfPermission(BookshelfApp.getBookshelfApp(), Manifest.permission.READ_EXTERNAL_STORAGE);
@@ -92,7 +98,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             } else {
                 Log.d(TAG, "getFileList(file)");
                 createAppFold();
-                startReadDBTask();
+                //startReadDBTask();
+                Message msg = handler.obtainMessage();
+                handler.sendMessage(msg);
             }
         }
     }
@@ -111,7 +119,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 Log.d(TAG,"onRequestPermissionsResult");
                 createAppFold();
-                startReadDBTask();
+                //startReadDBTask();
+                Message msg = handler.obtainMessage();
+                handler.sendMessage(msg);
             }
             else{
                 //如果不授权权限，则应该退出。
@@ -235,12 +245,84 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         books = null;
     }
 
+    private void readDB(){
+        List<MyBook> books = BookshelfApp.getBookshelfApp().getBooks();
+        books.clear();
+        BookDBHelper bookDBHelper = new BookDBHelper(BookshelfApp.getBookshelfApp());
+        SQLiteDatabase db = bookDBHelper.getReadableDatabase();
+        String[] columns = new String[]{"bookId","bookName","bookPath","bookCurrChapterIndx","charTotalCount"};
+        if(books.size() == 0){
+            Cursor cursor = db.query("bookTable", columns, null,null,null,null,null);
+            while(cursor.moveToNext()){
+                int bookId = cursor.getInt(cursor.getColumnIndex("bookId"));
+                Log.d(TAG,"bookId = "+bookId);
+                String bookName = cursor.getString(cursor.getColumnIndex("bookName"));
+                Log.d(TAG,"bookName ="+bookName);
+                String bookPath = cursor.getString(cursor.getColumnIndex("bookPath"));
+                Log.d(TAG,"bookPath ="+bookPath);
+                int bookCurrChapterIndx = cursor.getInt(cursor.getColumnIndex("bookCurrChapterIndx"));
+                Log.d(TAG,"bookCurrChapterIndx ="+bookCurrChapterIndx);
+                int charTotalCount = cursor.getInt(cursor.getColumnIndex("charTotalCount"));
+                MyBook mb = new MyBook();
+                mb.setName(bookName);
+                mb.setPath(bookPath);
+                mb.setCurrChapterIndx(bookCurrChapterIndx);
+                mb.setChapterList(this.getChapterList(db,bookId));
+                mb.setCharTotalCount(charTotalCount);
+                books.add(mb);
+            }
+            cursor.close();
+            bookDBHelper.closeDB(db);
+        }
+        Log.d(TAG,"bookSize="+BookshelfApp.getBookshelfApp().getBooks().size());
+    }
+
+//    cv.put("bookId",bookId);
+//    cv.put("chapterName",chapter.getName());
+//    cv.put("beginCharIndex",chapter.getBeginCharIndex());
+//    cv.put("beginContentIndex",chapter.getBeginContentIndex());
+//    cv.put("currPageNumIndx",0);
+
+    private List<Chapter> getChapterList(SQLiteDatabase db,int bookId){
+        List<Chapter> chapterList = new ArrayList<Chapter>();
+        String sBookId = String.valueOf(bookId);
+        String[] columns = new String[]{"chapterName","beginCharIndex","beginContentIndex","currPageNumIndx"};
+        Cursor cursor = db.query("chapterTable",columns,"bookId=?",new String[]{sBookId},null,null,null,null);
+        while(cursor.moveToNext()){
+            String chapterName = cursor.getString(cursor.getColumnIndex("chapterName"));
+            int beginCharIndex = cursor.getInt(cursor.getColumnIndex("beginCharIndex"));
+            int beginContentIndex = cursor.getInt(cursor.getColumnIndex("beginContentIndex"));
+            int currPageNumIndx = cursor.getInt(cursor.getColumnIndex("currPageNumIndx"));
+//            Log.d(TAG,"chaptename="+chapterName+"begincharindx="+beginCharIndex+"beginConindx="+beginContentIndex
+//           +"currpagenumindx="+currPageNumIndx);
+            Chapter chapter = new Chapter();
+            chapter.setName(chapterName);
+            chapter.setBeginCharIndex(beginCharIndex);
+            chapter.setBeginContentIndex(beginContentIndex);
+            chapter.setCurrPageNumIndx(currPageNumIndx);
+            chapterList.add(chapter);
+        }
+        cursor.close();
+        return chapterList;
+    }
+
+    final Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            readDB();
+            ((BookshelfGridAdapter)gv.getAdapter()).notifyDataSetChanged();
+        }
+    };
+
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG,"onResume");
         if(books == null){
             books = BookshelfApp.getBookshelfApp().getBooks();
-            this.startReadDBTask();
+            //this.startReadDBTask();
+            Message msg = handler.obtainMessage();
+            handler.sendMessage(msg);
         }
     }
 
@@ -249,5 +331,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onStop();
         //books = null;
         Log.d(TAG,"onStop");
+        books = null;
     }
+
 }
